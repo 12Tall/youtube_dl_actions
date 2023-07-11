6dfdbbb8df83512e233c32dbe25a27df0c667343
12tall <fb.ouyang@outlook.com>
2023-07-11 20:11:58 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo ./$1
 ./transfer anon $1 | head -n 2 | tail -n 1  
 ### get the 2nd line of `./transfer ***`, the result will be embeded in issue comment ###
 # `head -n 2`: get the first 2 lines of the outputs of `./transfer ***`                 
