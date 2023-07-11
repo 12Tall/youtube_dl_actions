911217941492b48091690f5631636a27b17e8805
12tall <fb.ouyang@outlook.com>
2023-07-11 21:11:08 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -4,8 +4,10 @@
 
 echo "Please download file in 14 days\n"  
 # ./transfer trs $1 | tail -n 1 
+./transfer muse $1 | tail -n 1  
 
-./transfer anon $1 | tail -n 1  
+# need VPN connection #
+# ./transfer anon $1 | tail -n 1  
 ### get the 2nd line of `./transfer ***`, the result will be embeded in issue comment ###
 # `head -n 2`: get the first 2 lines of the outputs of `./transfer ***`                 
 # `tail -n 1`: get the last 1 line of the outputs of `head -n 2`                        
