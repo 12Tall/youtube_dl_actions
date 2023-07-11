e609bb3023a4f221e27bbec7779de780c8f2f87a
12tall <fb.ouyang@outlook.com>
2023-07-11 17:15:20 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-./transfer anon ./downloads.zip | head -n 2 | tail -n 1
+./transfer anon ./downloads.zip | head -n 2 | tail -n 1  
+### get the 2nd line of `./transfer ***`, the result will be embeded in issue comment ###
+# `head -n 2`: get the first 2 lines of the outputs of `./transfer ***`                 
+# `tail -n 1`: get the last 1 line of the outputs of `head -n 2`                        
+
