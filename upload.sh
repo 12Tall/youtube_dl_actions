df5c8b77de7f2de257c222ab1d384b43fdeef596
12tall <fb.ouyang@outlook.com>
2023-07-11 20:48:03 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,7 +2,9 @@
 
 # echo ./$1 # for test  
 
-./transfer muse $1 | head -n 2 | tail -n 1  
+ ./transfer trs $1 | tail -n 1  
+
+# ./transfer anon $1 | head -n 2 | tail -n 1  
 ### get the 2nd line of `./transfer ***`, the result will be embeded in issue comment ###
 # `head -n 2`: get the first 2 lines of the outputs of `./transfer ***`                 
 # `tail -n 1`: get the last 1 line of the outputs of `head -n 2`                        
