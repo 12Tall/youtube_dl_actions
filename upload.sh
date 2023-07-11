8c6ff5763c80a209173071bba5afae3f12b5353b
12tall <fb.ouyang@outlook.com>
2023-07-11 21:01:41 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -3,9 +3,9 @@
 # echo ./$1 # for test  
 
 echo "Please download file in 14 days\n"  
- ./transfer trs $1 | tail -n 1 
+# ./transfer trs $1 | tail -n 1 
 
-# ./transfer anon $1 | head -n 2 | tail -n 1  
+./transfer anon $1 | tail -n 1  
 ### get the 2nd line of `./transfer ***`, the result will be embeded in issue comment ###
 # `head -n 2`: get the first 2 lines of the outputs of `./transfer ***`                 
 # `tail -n 1`: get the last 1 line of the outputs of `head -n 2`                        
